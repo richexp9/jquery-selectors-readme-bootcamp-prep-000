72f52b4a63e948a782828ca8b00d4d02ad5d83c2
Richard Loera <richexp9@gmx.com>
2017-12-03 08:54:04 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,19 @@
-// declare your functions here...
+function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+
+}
+
+function divSelector() {
+
+}
+
+function firstListItem() {
+
+}
